30415543ee3920f42b804aa1dd9c4f5f0da25469
tmnk <tmnk@github.com>
2019-02-16 02:02:29 +0300

    demo send return snackbar

--- lib/detail.dart
+++ lib/detail.dart
@@ -10,17 +10,37 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       body: GestureDetector(
         child: Column(
-            children: <Widget>[ Center(
-              child: Hero(
-                tag: 'imageHero',
-                child:
-                Image.network(
-                  'https://picsum.photos/250?image=9',
+            children: <Widget>[
+              Center(
+                child: Hero(
+                  tag: 'imageHero',
+                  child:
+                  Image.network(
+                    'https://picsum.photos/250?image=9',
+                  ),
                 ),
               ),
-            ),
-            Center(
-                child : Text ('Hello ${testValue * 10}'))
+              Center(
+                child : Text ('Hello ${testValue * 10}')
+              ),
+              Padding(
+                child : SizedBox(
+                  width: 100,
+                  height: 100,
+
+                  child: Center(child:
+                  RaisedButton(
+                    color : Colors.lightGreenAccent,
+                    onPressed: () {
+                      Navigator.pop(context, 'Yep!');
+                    },
+
+                    child: Center(child: Text('EN')),
+                  ),
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+              ),
 
             ]
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,55 @@
 import 'package:flutter/material.dart';
 import './detail.dart';
-void main() => runApp(HeroApp());
-
-class HeroApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Transition Demo',
-      home: MainScreen(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    title: 'Returning Data',
+    home: HomeScreen(),
+  ));
 }
 
-class MainScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Screen'),
+        title: Text('Returning Data Demo'),
       ),
-      body: GestureDetector(
-        child: Hero(
-          tag: 'imageHero',
-          child: Image.network(
-            'https://picsum.photos/250?image=9',
-          ),
-        ),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => DetailScreen(testValue : 1),
-            ),
-          );
-        },
-      ),
+      body: Center(child: SelectionButton()),
     );
+  }
+}
+
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  GestureDetector(
+      child: Hero(
+        tag: 'imageHero',
+        child: Image.network(
+          'https://picsum.photos/250?image=9',
+        ),
+      ),
+      onTap: () {
+        _navigateAndDisplaySelection(context);
+      },
+
+    );
+  }
+
+  // A method that launches the SelectionScreen and awaits the result from
+  // Navigator.pop!
+  _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that will complete after we call
+    // Navigator.pop on the Selection Screen!
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => DetailScreen(testValue : 1)),
+    );
+
+    // After the Selection Screen returns a result, hide any previous snackbars
+    // and show the new result!
+    Scaffold.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text("$result")));
   }
 }
